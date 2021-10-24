9a50ad5b3ddbb5d2716417feb1dfca65ceffc65f
SSKRI <61083267+SSKRI@users.noreply.github.com>
2021-10-24 19:15:55 +0300

    layout from psd in Marsy

---
+++ JS/2021/Practice/minin/Less3-JS-basis/Less3-JS-basis/js/script.js
@@ -0,0 +1,22 @@
+// const age = prompt("сколько тебе лет?");
+// const userName = prompt("как твое имя?");
+
+// alert("Привет " + userName + ", тeбе " + age + " лет");
+
+// let number = 45;
+// console.log(number);
+// console.log("мфмф" + number);
+// console.log(number++);
+// console.log(++number);
+
+const number = true;
+const number2 = "bbb";
+const number3 = 7;
+const number4 = null;
+let x;
+console.log(typeof number);
+console.log(typeof number2);
+console.log(typeof number3);
+console.log(typeof number4);
+console.log(null);
+console.log(typeof x);
